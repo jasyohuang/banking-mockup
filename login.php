4697102b807ab281640c7efd4ac4a213ce445b01
jasyohuang <jasonyonathan@live.com>
2022-06-09 10:50:35 +0700

    added callsign SDK

--- login.php
+++ login.php
@@ -38,17 +38,18 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="script/script.js"></script>
     <title>Login :)</title>
 </head>
-<body>
+<body onload="startWebSDK()">
     <div class="container">
         <form action="" method="POST" class="login-email">
             <h1>Login</h1>
             <div class="form-outline mb-4">
-                <input type="email" class="form-control" id="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+                <input type="email" class="form-control csu-username" id="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
             </div>
             <div class="form-outline mb-4">
-                <input class="form-control" id="password" type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+                <input class="form-control csu-password" id="password" type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="form-outline mb-4">
                 <button name="submit" class="btn btn-primary btn-block mb-4">Login</button>
@@ -58,3 +59,38 @@ if (isset($_POST['submit'])) {
     </div>
 </body>
 </html>
+
+<script>
+            sessionId = "sid-" + uuidv4();
+            transactionId = "txn-" + uuidv4();
+            setCookie("sessionId", sessionId, 1);
+
+            // orgId_replacement: replace with your own org Id in conf\conf.js
+            // Normally this starts with org-
+            // It can be found by logging into the Policy Manager and clicking Enterprise Manager from the drop down
+            orgId = "org-1fw2ahh4-rjrnm2yg-k290795ks-4p306jmb";
+
+            // intelligenceEngineEndpoint_replacement: replace with the supplied intelligence engine endpoint in n conf\conf.js
+            // Normally for EU MTE this is: https://app.s01.callsign.com
+            intelligenceEngineEndPoint = "https://app.s01.callsign.com"; 
+
+            function startWebSDK (){
+                const config = {
+                    essx: intelligenceEngineEndPoint, 
+                    esto: orgId, 
+                    ggow: transactionId,  
+                    mosc: sessionId,  
+                    mwel: "PrimaryLogin", 
+                    mwelsub: "Username", 
+                    mwelseq: 1, 
+                    reed : true,
+                    sanf: JSON.stringify({ "#my-login-form": "submit" }), 
+                    time: Date.now()
+                };
+                sessionStorage.setItem('cx', JSON.stringify(config));
+            }
+
+            
+        </script>
+
+        <script src="https://app.s01.callsign.com/in/web-sdk/v1/static/web-sdk.js"></script>  
